93d785136ac3cb530be51c0a97fb24b95fa917f1
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-05-26 11:08:01 +0200

    refactor: util add delay fn

--- libs/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BookingStore, Flight, FlightFilter } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
+import { addDelay } from '../../util-flight/add-delay';
 
 
 @Component({
@@ -20,15 +21,7 @@ export class FlightSearchComponent {
   protected store = inject(BookingStore);
 
   protected delay(flight: Flight): void {
-    const oldFlight = flight;
-    const oldDate = new Date(oldFlight.date);
-
-    const newDate = new Date(oldDate.getTime() + 1000 * 60 * 5); // Add 5 min
-    const newFlight = {
-      ...oldFlight,
-      date: newDate.toISOString(),
-      delayed: true
-    };
+    const newFlight = addDelay(flight);
 
     const flights = this.store.flights().map(
       flight => flight.id === newFlight.id ? newFlight : flight
